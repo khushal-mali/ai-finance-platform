9192270cc5b30369bfb6da949da80d631931a998
Khushal Mali <khushalmali8448@gmail.com>
2025-08-24 15:47:34 +0530

    added database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import cors from "cors";
 import { HTTPSTATUS } from "./config/http.config.js";
 import { errorHandler } from "./middlewares/errorHandler.middleware.js";
 import { BadRequestException } from "./utils/app-error.js";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware.js";
+import connectDatabase from "./config/database.config.js";
 
 const app = express();
 const BASE_PATH = Env.BASE_PATH;
@@ -18,15 +20,19 @@ app.use(
   })
 );
 
-app.get("/", (req, res, next) => {
-  throw new BadRequestException("This is a bad request");
-  return res.status(HTTPSTATUS.OK).json({
-    message: "Hello! I am server.",
-  });
-});
+app.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Hello! I am server.",
+    });
+  })
+);
 
 app.use(errorHandler);
 
-app.listen(Env.PORT, () => {
+app.listen(Env.PORT, async () => {
+  await connectDatabase();
+
   console.log(`server running on port --${Env.PORT} on ${Env.NODE_ENV} mode`);
 });
