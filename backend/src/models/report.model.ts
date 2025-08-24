deedf5231f58e1bc519280e9c6be8046015fcf5c
Khushal Mali <khushalmali8448@gmail.com>
2025-08-24 20:16:50 +0530

    reports schema

---
+++ backend/src/models/report.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document } from "mongoose";
+
+export enum ReportStatusEnum {
+  "SENT" = "SENT",
+  "PENDING" = "PENDING",
+  "FAILED" = "FAILED",
+}
+
+export interface ReportDocument extends Document {
+  userId: mongoose.Types.ObjectId;
+  period: String;
+  sentDate: Date;
+  status: keyof typeof ReportStatusEnum;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const reportSchema = new mongoose.Schema<ReportDocument>(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+    period: { type: String, required: true },
+    sentDate: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: Object.values(ReportStatusEnum),
+      default: ReportStatusEnum.PENDING,
+    },
+  },
+  { timestamps: true }
+);
+
+const ReportModel = mongoose.model<ReportDocument>("Report", reportSchema);
+
+export default ReportModel;
